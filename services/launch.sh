d2352aa63ef23b522ced87c4584ff3fa4b0ad5dc
Will Barnett <will.barnett@kcl.ac.uk>
2023-05-12 14:33:50 +0100

    Updated launch script following epsilon complete integration

--- services/launch.sh
+++ services/launch.sh
@@ -4,11 +4,9 @@ gnome-terminal -- /bin/sh -c 'cd com.mde-network.ep.toolfunctions.eclipseoclfunc
 
 gnome-terminal -- /bin/sh -c 'cd com.mde-network.ep.toolfunctions.emfaticfunction && mvn function:run -Drun.functionTarget=com.mdenetnetwork.ep.toolfunctions.emfaticfunction.RunConversionEcoreToEmfatic -Drun.port=9091'
 
-
 gnome-terminal -- /bin/sh -c 'cd com.mde-network.ep.toolfunctions.emfaticfunction && mvn function:run -Drun.functionTarget=com.mdenetnetwork.ep.toolfunctions.emfaticfunction.RunConversionEmfaticToDiagram -Drun.port=9092'
 
 gnome-terminal -- /bin/sh -c 'cd com.mde-network.ep.toolfunctions.epsilonfunction && mvn function:run -Drun.functionTarget=com.mdenetnetwork.ep.toolfunctions.epsilonfunction.RunConversionFlexmiToXmi -Drun.port=9093'
 
 
-gnome-terminal -- /bin/sh -c 'cd $HOME/git/epsilon/playground-docker && docker compose up'
 
