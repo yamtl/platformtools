b6c9c599a181fca2296603d97832e2ff5c2f37e0
Will Barnett <will.barnett@kcl.ac.uk>
2023-09-04 10:41:07 +0100

    Added the Xtext tool service to its container.

--- xtext/editorserver/src/config.js
+++ xtext/editorserver/src/config.js
@@ -2,9 +2,9 @@
 
 export const config = {
 
-    port: process.env.PORT || "10001",
+    port: process.env.ES_PORT || "10000",
 
-    endpointsPrefix: process.env.TS_ENDPOINT_PREFIX || "",
+    endpointsPrefix: process.env.ES_ENDPOINT_PREFIX || "",
 
     trustedWebOrigins: ['http://127.0.0.1:8080'],
 
--- xtext/start.sh
+++ xtext/start.sh
@@ -4,6 +4,12 @@
 # start tomcat
 catalina.sh run &
 
+# start toolfunction
+(cd /toolservice/com.mde-network.ep.toolfunctions.xtextfunction; \
+    mvn function:run \
+    -Drun.functionTarget=com.mdenetnetwork.ep.toolfunctions.xtextfunction.RunXtextFunction \
+    -Drun.port=$TS_PORT) &
+
 # start editorserver
 node ./src/server.js &
 
